77b7a9a3f7b139acf996a5e0a5677f49df6d9ec0
Maxwell A. Borden <maxwellborden@gmail.com>
2017-06-03 16:59:13 -0700

    yoooooo

--- templates/page_template.js
+++ templates/page_template.js
@@ -2,6 +2,7 @@ module.exports = (body, title) => `<!DOCTYPE html>
         <html lang="en">
             <head>
                 <meta charset="UTF-8">
+                <link rel="Stylesheet" href="/static/style.css">
                 <title>${title?title:"default title"}</title>
             </head>
             <body>${body}</body>
